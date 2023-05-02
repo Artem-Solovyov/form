3eb126d8b833b4933c6f75d064df7e4a6c559b5e
HP-UltraBook <solovevarem892@gmail.com>
2023-05-02 22:56:35 +0300

    2

--- sendmail.php
+++ sendmail.php
@@ -1,69 +1,69 @@
 <?php
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-    require 'path/to/PHPMailer/src/Exception.php';
-    require 'path/to/PHPMailer/src/PHPMailer.php';
+require 'path/to/PHPMailer/src/Exception.php';
+require 'path/to/PHPMailer/src/PHPMailer.php';
 
 
-    $mail = new PHPMailer(true);
-    $mail->CharSet = 'UTF-8';
-    $mail->setLanguage('ru', 'phpmailer/language/');
-    $mail->IsHTML(true);
+$mail = new PHPMailer(true);
+$mail->CharSet = 'UTF-8';
+$mail->setLanguage('ru', 'phpmailer/language/');
+$mail->IsHTML(true);
 
-    // от кого письмо
-    $mail->setForm('solowayit@gmail.com', 'Artem Solovyov');
-    // to
-    $mail->addAddress('solovevartem892@gmail.com');
-    // Theme
-    $mail->Subject = 'Hello World!';
+// от кого письмо
+$mail->setForm('solowayit@gmail.com', 'Artem Solovyov');
+// to
+$mail->addAddress('solovevartem892@gmail.com');
+// Theme
+$mail->Subject = 'Hello World!';
 
-    // Hand
-    #hand = "Right";
-    if($_POST['hand'] == "left"){
-        $hand = "left"
+// Hand
+#hand = "Right";
+if($_POST['hand'] == "left"){
+    $hand = "left";
+}
+
+// body of letter
+$body = '<h1>Super letter!</h1>';
+
+if(trim(!empty($_POST['name']))){
+    $body.='<p><strong>Name:</strong> '.$_POST['name'].'</p>';
+}
+if(trim(!empty($_POST['email']))){
+    $body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+}
+if(trim(!empty($_POST['hand']))){
+    $body.='<p><strong>Hand:</strong> '.$hand.'</p>';
+}
+if(trim(!empty($_POST['age']))){
+    $body.='<p><strong>Age:</strong> '.$_POST['age'].'</p>';
+}
+if(trim(!empty($_POST['message']))){
+    $body.='<p><strong>Message:</strong> '.$_POST['message'].'</p>';
+}
+
+//file
+if(!empty($_FILES['image']['tmp_name'])){
+    $filePath = __DIR__ . "/files/" . $_FILES['image']['name'];
+
+    if(copy($_FILES['image']['tmp_name'], $filePath)){
+        $fileAttach = $filePath;
+        $body.='<p><strong>Photo</strong></p>';
+        $mail->addAttachment($fileAttach);
     }
+}
 
-    // body of letter
-    $body = '<h1>Super letter!</h1>';
+$mail->Body = $body;
 
-    if(trim(!empty($_POST['name']))){
-        $body.='<p><strong>Name:</strong> '.$_POST['name'].'</p>';
-    }
-    if(trim(!empty($_POST['email']))){
-        $body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
-    }
-    if(trim(!empty($_POST['hand']))){
-        $body.='<p><strong>Hand:</strong> '.$hand.'</p>';
-    }
-    if(trim(!empty($_POST['age']))){
-        $body.='<p><strong>Age:</strong> '.$_POST['age'].'</p>';
-    }
-    if(trim(!empty($_POST['message']))){
-        $body.='<p><strong>Message:</strong> '.$_POST['message'].'</p>';
-    }
+if(!$mail->send()){
+    $message = 'Error';
+} else {
+    $message = 'Okay';
+}
 
-    //file
-    if(!empty($_FILES['image']['tmp_name'])){
-        $filePath = __DIR__ . "/files/" . $_FILES['image']['name'];
+$response = ['message' => $message];
 
-        if(copy($_FILES['image']['tmp_name'], $filePath)){
-            $fileAttach = $filePath;
-            $body.='<p><strong>Photo</strong></p>';
-            $mail->addAttachment($fileAttach);
-        }
-    }
-
-    $mail->Body = $body;
-
-    if(!$mail->send()){
-        $message = 'Error';
-    } else {
-        $message = 'Okay'
-    }
-
-    $response = ['message' => $message];
-
-    header('Content-type: application/json');
-    echo json_encode($response);
+header('Content-type: application/json');
+echo json_encode($response);
 ?>
