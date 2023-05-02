6b8c3f1900371df4d7cd577d4ba770e290a6afa9
HP-UltraBook <solovevarem892@gmail.com>
2023-05-02 22:27:46 +0300

    init

---
+++ main.js
@@ -0,0 +1,109 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("form");
+
+  form.addEventListener("submit", formSend);
+
+  async function formSend(e) {
+    e.preventDefault();
+
+    const error = formValidate(form);
+
+    let formData = new FormData(form);
+    formData.append("image", formImage.files[0]);
+
+    if (error === 0) {
+      form.classList.add("_sending");
+      let response = await fetch("sendmail.php", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        let result = await response.json();
+        alert(result.message);
+
+        formPerview.innerHTML = "";
+
+        form.reset();
+
+        form.classList.remove("_sending");
+      } else {
+        alert("Error");
+        form.classList.remove("_sending");
+      }
+    } else {
+      alert("Fill in required fields");
+    }
+  }
+
+  function formValidate(form) {
+    let error = 0;
+    let formReq = document.querySelectorAll("._req");
+
+    formReq.forEach((input, index) => {
+      formRemoveError(input);
+
+      if (input.classList.contains("_email")) {
+        if (emailTest(input)) {
+          formAddError(input);
+          error++;
+        }
+      } else if (
+        input.getAttribute("type") === "checkbox" &&
+        input.checked === false
+      ) {
+        formAddError(input);
+        error++;
+      } else {
+        if (input.value === "") {
+          formAddError(input);
+          error++;
+        }
+      }
+    });
+    return error;
+  }
+  // add class error
+  function formAddError(input) {
+    input.parentElement.classList.add("_error");
+    input.classList.add("_error");
+  }
+  // remove class error
+  function formRemoveError(input) {
+    input.parentElement.classList.remove("_error");
+    input.classList.remove("_error");
+  }
+  // Email validation
+  function emailTest(input) {
+    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+  }
+
+  // Input file (image)
+  const formImage = document.getElementById("formImage");
+  const formPerview = document.getElementById("formPerview");
+
+  formImage.addEventListener("change", () => {
+    uploadFile(formImage.files[0]);
+  });
+
+  function uploadFile(file) {
+    if (!["image/jpeg", "image/png", "image/gif"].includes(file.type)) {
+      alert("Only images");
+      formImage.value = "";
+      return;
+    }
+    if (file.size > 2 * 1024 * 1024) {
+      alert("File larger than 2 mb");
+      return;
+    }
+
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      formPerview.innerHTML = `<img src="${e.target.result}" alt="Image">`;
+    };
+    reader.onerror = function (e) {
+      alert("Error");
+    };
+    reader.readAsDataURL(file);
+  }
+});
---
+++ sendmail.php
@@ -0,0 +1,68 @@
+<?php
+    use PHPMailer\PHPmailer\PHPmailer;
+    use PHPMailer\PHPmailer\Exception;
+
+    require 'phpmailer/src/Exception.php';
+    require 'phpmailer/src/PHPmailer.php';
+
+    $mail = new PHPMailer(true);
+    $mail->CharSet = 'UTF-8';
+    $mail->setLanguage('ru', 'phpmailer/language/');
+    $mail->IsHTML(true);
+
+    // от кого письмо
+    $mail->setForm('solowayit@gmail.com', 'Artem Solovyov');
+    // to
+    $mail->addAddress('solovevartem892@gmail.com');
+    // Theme
+    $mail->Subject = 'Hello World!';
+
+    // Hand
+    #hand = "Right";
+    if($_POST['hand'] == "left"){
+        $hand = "left"
+    }
+
+    // body of letter
+    $body = '<h1>Super letter!</h1>';
+
+    if(trim(!empty($_POST['name']))){
+        $body.='<p><strong>Name:</strong> '.$_POST['name'].'</p>';
+    }
+    if(trim(!empty($_POST['email']))){
+        $body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+    }
+    if(trim(!empty($_POST['hand']))){
+        $body.='<p><strong>Hand:</strong> '.$hand.'</p>';
+    }
+    if(trim(!empty($_POST['age']))){
+        $body.='<p><strong>Age:</strong> '.$_POST['age'].'</p>';
+    }
+    if(trim(!empty($_POST['message']))){
+        $body.='<p><strong>Message:</strong> '.$_POST['message'].'</p>';
+    }
+
+    //file
+    if(!empty($_FILES['image']['tmp_name'])){
+        $filePath = __DIR__ . "/files/" . $_FILES['image']['name'];
+
+        if(copy($_FILES['image']['tmp_name'], $filePath)){
+            $fileAttach = $filePath;
+            $body.='<p><strong>Photo</strong></p>';
+            $mail->addAttachment($fileAttach);
+        }
+    }
+
+    $mail->Body = $body;
+
+    if(!$mail->send()){
+        $message = 'Error';
+    } else {
+        $message = 'Okay'
+    }
+
+    $response = ['message' => $message];
+
+    header('Content-type: application/json');
+    echo json_encode($response);
+?>
